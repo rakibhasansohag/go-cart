458c1237934ebfdbcaaecf9476ee030c5e9fde45
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-07-23 23:19:42 +0600

    added shadcn as component library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
+import { Button } from '../components/ui/button';
 
 export default function Home() {
-  return (
+	return (
 		<div>
 			<h1 className='text-3xl text-bold text-red-500 underline font-barlowFont'>
 				Hello Go Cart{' '}
 			</h1>
+
+			<Button variant={'default'} className='font-barlowFont mt-2'>
+				Click Me
+			</Button>
 		</div>
 	);
 }
