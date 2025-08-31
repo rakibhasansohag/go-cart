43941ad0f6a34b0a9530150fc39905c4df96efd4
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-09-01 03:05:08 +0600

    categories header added both offer tag and categories

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from '../../components/store/layout/header/header';
-
+import Header from '@/components/store/layout/header/header';
+import CategoriesHeader from '@/components/store/layout/categories-header/categories-header';
 
 async function HomePage() {
 	return (
 		<>
 			<Header />
+			<CategoriesHeader />
 		</>
 	);
 }
