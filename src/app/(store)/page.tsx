08086480c554841fa4d541ef287d5b568678d078
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-09-01 20:21:28 +0600

    footer added

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '@/components/store/layout/header/header';
 import CategoriesHeader from '@/components/store/layout/categories-header/categories-header';
+import Footer from '@/components/store/layout/footer/footer';
 
 async function HomePage() {
 	return (
 		<>
 			<Header />
 			<CategoriesHeader />
+
+			<Footer />
 		</>
 	);
 }
