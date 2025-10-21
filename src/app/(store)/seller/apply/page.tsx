1cc9b9ac6ede45474c12332606c22d3967a9a0ae
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-10-21 21:00:17 +0600

    apply for seller page added  only the header page

---
+++ src/app/(store)/seller/apply/page.tsx
@@ -0,0 +1,9 @@
+import MinimalHeader from '@/components/store/layout/minimal-header/header';
+
+export default function SellerApplyPage() {
+	return (
+		<div className='bg-[#eef4fc] dark:bg-[#191520] h-screen overflow-y-hidden'>
+			<MinimalHeader />
+		</div>
+	);
+}
