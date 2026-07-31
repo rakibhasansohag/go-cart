420d3f8938d6ff7dcdfd8adfae662876731df1c8
Rakib Hasan Sohag <rakibhasansohag133@gmail.com>
2026-07-31 23:10:07 +0600

    refactor: streamline EmptyCart component by reorganizing imports, improving layout structure, and enhancing button accessibility

--- src/components/store/cart-page/empty-cart.tsx
+++ src/components/store/cart-page/empty-cart.tsx
@@ -1,27 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '../ui/button';
 export default function EmptyCart() {
-	return (
-		<div className='bg-f5 w-full mx-auto px-4 text-center'>
-			<div className='min-h-[calc(100vh-65px)] pb-14 flex flex-col justify-center items-center'>
-				<Image
-					src='/assets/images/cart.avif'
-					alt='Cart image'
-					width={300}
-					height={300}
-					className='w-64 h-64'
-				/>
-				<span className='py-4 font-bold my-3'>
-					No items yet? Continue shopping to explore more.
-				</span>
-				<Link href='/browse'>
-					<Button variant='pink' className='w-56'>
-						Explore items
-					</Button>
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className="mx-auto w-full bg-f5 px-4 text-center text-main-primary">
+      <div className="min-h-[calc(100vh-65px)] pb-14 flex flex-col justify-center items-center">
+        <Image
+          src="/assets/images/cart.avif"
+          alt="Cart image"
+          width={300}
+          height={300}
+          className="w-64 h-64"
+        />
+        <span className="py-4 font-bold my-3">
+          No items yet? Continue shopping to explore more.
+        </span>
+        <Button asChild size="lg" className="w-56 rounded-full">
+          <Link href="/browse">Explore items</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
