7841e3fe453a93fe4bb3a12c4be37a68eb8ba6fc
Rakib Hasan Sohag <rakibhasansohag133@gmail.com>
2026-09-14 19:16:21 +0600

    feat: add HomeMainSwiper component for store homepage

--- src/components/store/home/main/home-swiper.tsx
+++ src/components/store/home/main/home-swiper.tsx
@@ -1,49 +1,69 @@
 'use client';
-import AwesomeSlider from 'react-awesome-slider';
-import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import 'react-awesome-slider/dist/styles.css';
-const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, EffectFade } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+
 import Img1 from '@/public/assets/images/swiper/1.webp';
 import Img2 from '@/public/assets/images/swiper/2.webp';
 import Img3 from '@/public/assets/images/swiper/3.webp';
 import Img4 from '@/public/assets/images/swiper/4.webp';
-import Image from 'next/image';
-
-export default function HomeMainSwiper() {
-	return (
-		<div>
-			<AutoplaySlider
-				animation='cubeAnimation'
-				bullets={false}
-				play={true}
-				cancelOnInteraction={false}
-				interval={6000}
-			>
-				{images.map((img) => (
-					<div key={img.id}>
-						<Image src={img.url} alt='' />
-					</div>
-				))}
-			</AutoplaySlider>
-		</div>
-	);
-}
 
 const images = [
 	{
 		id: 1,
 		url: Img1,
+		alt: 'Winter Adventure Wear Banner 1',
 	},
 	{
 		id: 2,
 		url: Img2,
+		alt: 'Winter Adventure Wear Banner 2',
 	},
 	{
 		id: 3,
 		url: Img3,
+		alt: 'Winter Adventure Wear Banner 3',
 	},
 	{
 		id: 4,
 		url: Img4,
+		alt: 'Winter Adventure Wear Banner 4',
 	},
 ];
+
+export default function HomeMainSwiper() {
+	return (
+		<div className='w-full overflow-hidden rounded-md'>
+			<Swiper
+				modules={[Autoplay, Pagination, EffectFade]}
+				effect='fade'
+				fadeEffect={{ crossFade: true }}
+				autoplay={{
+					delay: 6000,
+					disableOnInteraction: false,
+				}}
+				pagination={{
+					clickable: true,
+				}}
+				loop={true}
+				className='w-full rounded-md'
+			>
+				{images.map((img, index) => (
+					<SwiperSlide key={img.id} className='rounded-md overflow-hidden'>
+						<Image
+							src={img.url}
+							alt={img.alt}
+							className='w-full h-auto object-cover rounded-md'
+							priority={index === 0}
+							loading={index === 0 ? 'eager' : 'lazy'}
+						/>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+}
